37657eaad629622d23fb79e1b3e28e2d1658d82a
Monge1h <jlo4321jl@gmail.com>
2019-12-01 23:46:59 -0600

    Add helpers

---
+++ src/helpers/helper.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /**
+     *  Add IdTeam to each hacker
+     * @param {Array} arrayOfHackers 
+     * @param {Number} idTeam 
+     * @returns {Array} New array with arrays inside that contains idHacker and idTeam
+     */
+    addIdTeam: (arrayOfHackers, idTeam) => {
+        return arrayOfHackers.map((hacker, i) => [hacker[i], idTeam]);
+    }
+}
